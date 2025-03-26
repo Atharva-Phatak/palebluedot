9819db50e6ea04b761afe9ba0cbe68c35019334e
Atharva-Phatak <athp456@gmail.com>
2025-03-26 17:22:38 -0400

    added build push actions

--- pbd/pipelines/data_collection/collect_data.py
+++ pbd/pipelines/data_collection/collect_data.py
@@ -1,15 +1,14 @@
-from pbd.steps.data_collection.page_search import extract_pdf_links
-from pbd.steps.data_collection.download import download_books
+from pbd.pipelines.data_collection.steps.page_search import extract_pdf_links
+from pbd.pipelines.data_collection.steps.download import download_books
 from zenml import pipeline
 from minio import Minio
 
 @pipeline(name = "data_collection_pipeline",
-          enable_step_logs=True)
+          )
 def data_collection_pipeline():
-    minio_access_key = "minio@1234"
-    minio_secret_key = "minio@local1234"
-    minio_ingress_host = "http://fsml-minio.info"
-    client = Minio(endpoint=minio_ingress_host,
-                   access_key=minio_access_key,
-                   secret_key=minio_secret_key,)
-    pdf_links = extract_pdf_links()
+    pdf_links = extract_pdf_links(book_name="The Grand Design, Stephen Hawking")
+    download_books(books_to_download=pdf_links,
+                   bucket_name="data-bucket")
+
+if __name__ == "__main__":
+    data_collection_pipeline()
--- pbd/pipelines/data_collection/steps/validation.py
+++ pbd/pipelines/data_collection/steps/validation.py
@@ -4,4 +4,4 @@ import pydantic
 class ExtractedData(pydantic.BaseModel):
     title: str
     extension: str
-    links: str
+    links: list[str]
