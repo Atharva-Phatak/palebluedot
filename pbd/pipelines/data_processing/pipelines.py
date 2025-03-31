4f84f2be26c134fde9c4bfaa577dd9abd78d98c7
Atharva-Phatak <athp456@gmail.com>
2025-03-31 17:58:55 -0400

    remove step operator

--- pbd/pipelines/data_processing/pipelines.py
+++ pbd/pipelines/data_processing/pipelines.py
@@ -2,7 +2,7 @@ from zenml import pipeline
 from pbd.pipelines.data_processing.steps.data_process import download_youtube_audio
 from pbd.pipelines.data_processing.setting import docker_settings
 
-@pipeline(name = "collect_audio_data",settings = {"docker: docker_settings"})
+@pipeline(name = "collect_audio_data",settings = {"docker": docker_settings})
 def collect_audio_data(url:str):
     download_youtube_audio(url = url)
 
