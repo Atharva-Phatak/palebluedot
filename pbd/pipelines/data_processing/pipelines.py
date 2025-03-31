3f7731253e9aa6998d3115bdcb45b127bf6b0b33
Atharva-Phatak <athp456@gmail.com>
2025-03-30 21:11:08 -0400

    Add dummy url

--- pbd/pipelines/data_processing/pipelines.py
+++ pbd/pipelines/data_processing/pipelines.py
@@ -4,3 +4,7 @@ from pbd.pipelines.data_processing.steps.data_process import download_youtube_au
 @pipeline(name = "collect_audio_data")
 def collect_audio_data(url:str):
     download_youtube_audio(url = url)
+
+
+if __name__ == "__main__":
+    collect_audio_data(url = "https://www.youtube.com/watch?v=wupToqz1e2g")
