31e584307187d48dc7430737814a445b895ff0bf
Atharva Phatak <athp456@gmail.com>
2025-05-18 06:37:31 -0400

    add inference code

---
+++ pbd/pipelines/fast_inference/pipeline.py
@@ -0,0 +1,40 @@
+from zenml.client import Client
+from zenml.config import load_config
+from zenml import pipeline
+from components.downloader import download_from_minio
+from components.extractor import extract_zip
+from components.ocr_processor import ocr_images
+import os
+@pipeline
+def ocr_pipeline(
+    endpoint: str,
+    bucket: str,
+    object_key: str,
+    local_path: str,
+    extract_to: str,
+    model_config: dict,
+    generation_config: dict,
+    prompt: str,
+):
+    access_key = os.environ.get("AWS_ACCESS_KEY_ID")
+    secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    if not access_key or not secret_key:
+        raise ValueError("AWS credentials not found in environment variables.")
+    zip_path = download_from_minio(
+        endpoint=endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        bucket=bucket,
+        object_key=object_key,
+        local_path=local_path
+    )
+    image_paths = extract_zip(zip_path=zip_path, extract_to=extract_to)
+    ocr_images(
+        image_paths=image_paths,
+        model_config=model_config,
+        generation_config=generation_config,
+        prompt=prompt,
+    )
+
+if __name__ == "__main__":
+    ocr_pipeline.with_options(config_path = "./config/config.yaml")()
