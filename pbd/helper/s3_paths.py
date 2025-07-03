65fd07812ecca000968acac58a22bec332370333
Atharva-Phatak <athp456@gmail.com>
2025-07-03 17:54:03 -0400

    fix paths

--- pbd/helper/s3_paths.py
+++ pbd/helper/s3_paths.py
@@ -1,6 +1,6 @@
 def data_processing_pipeline_config_path():
-    return "pipeline_configs/data_processing/config.yaml"
+    return "pipeline_configs/data_processing/config.json"
 
 
 def ocr_engine_config_path():
-    return "pipeline_configs/ocr_engine/config.yaml"
+    return "pipeline_configs/ocr_engine/config.json"
