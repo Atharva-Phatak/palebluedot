aa405101a19ad2a1a1dc3b770027e2549cd6189d
Atharva-Phatak <athp456@gmail.com>
2025-09-03 13:26:58 -0400

    add flyte workflow

--- pbd/pipelines/flyte_test/pipeline.py
+++ pbd/pipelines/flyte_test/pipeline.py
@@ -6,6 +6,11 @@ from flytekit import task, workflow, Resources
     container_image="ghcr.io/atharva-phatak/pbd-flyte_test:latest",
     requests=Resources(cpu="1", mem="512Mi"),
     limits=Resources(cpu="2", mem="1Gi"),
+    environment={
+        "AWS_ACCESS_KEY_ID": "minio@1234",
+        "AWS_SECRET_ACCESS_KEY": "minio@local1234",
+        "AWS_REGION": "us-east-1",
+    },
 )
 def compute_square(x: int) -> int:
     return x * x
